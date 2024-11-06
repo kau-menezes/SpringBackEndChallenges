2fa451ae69d44413dfc31c78c0e523a404d35102
akauzinha47 <kau.vmenezes@gmail.com>
2024-11-06 11:56:55 -0300

    problems solved

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.dto.UserData;
 import com.example.demo.dto.UserUpdatePassword;
 import com.example.demo.services.UserService;
-
-import java.util.*;
 
 @RestController
 public class UserController {
