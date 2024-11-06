1eca11213e3477bf5390f308ce8916278725ee9e
akauzinha47 <kau.vmenezes@gmail.com>
2024-11-06 11:07:27 -0300

    user model for C6 and beyond exercises

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+public class UserController {
+    
+}
