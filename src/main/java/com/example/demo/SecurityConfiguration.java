8164dd3e453db0913d3ce17e7539efcabab7bada
akauzinha47 <kau.vmenezes@gmail.com>
2024-11-07 10:03:12 -0300

    #C9 - security config update + jwt filtet

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,23 +1,34 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.example.demo.dto.Token;
+import com.example.demo.filter.JWTAuthenticationFilter;
+import com.example.demo.services.JWTService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    
+    @Autowired
+    JWTService<Token> jwtService;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
-                .anyRequest().permitAll()
+                .requestMatchers("/user").permitAll()
+                .requestMatchers("/user/login").permitAll()
+                .anyRequest().authenticated()
             )
+            .addFilterBefore(new JWTAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
             .build();
     }
-    
 }
